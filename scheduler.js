161ef6a0fdf1136a8bc7d13082bdd2a023f67534
Sacha Rodier <sacharodier94@googlemail.com>
2020-08-19 18:27:00 +0200

    asdasd

--- scheduler.js
+++ scheduler.js
@@ -8,17 +8,3 @@ const text = document.querySelector('[data-text]');
 
 image.src = `./assets/images/${latestMsg.image}`;
 text.innerHTML = latestMsg.text;
-
-const box = document.querySelector('[data-box]');
-const container = document.querySelector('[data-container]');
-
-messages.forEach((element) => {
-  let clone = box.cloneNode(true);
-  let cloneImage = clone.querySelector('[data-image]');
-  let cloneText = clone.querySelector('[data-text]');
-
-  cloneImage.src = `./assets/images/${element.image}`;
-  cloneText.innerHTML = element.text;
-
-  container.appendChild(clone);
-});
