b8f401da2a13939703775c6191b1e88799de0520
Sacha Rodier <sacharodier94@googlemail.com>
2020-08-19 23:05:24 +0200

    oops

--- scheduler.js
+++ scheduler.js
@@ -1,7 +1,7 @@
 // sort 'em first to be safe.
 messages.sort((first, second) => new Date(first.date) - new Date(second.date));
 
-const passedMsgs = messages.filter((elem) => new Date(elem.date) < new Date('2021-01-11T01:00:00.000Z'));
+const passedMsgs = messages.filter((elem) => new Date(elem.date) < new Date());
 const latestMsg = passedMsgs.slice(-1)[0];
 const nextMsg = messages[messages.indexOf(latestMsg) + 1];
 const localeNextDate = new Date(nextMsg.date).toLocaleDateString('de-DE');
